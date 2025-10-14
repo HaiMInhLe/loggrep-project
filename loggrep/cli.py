6f4ede57b54035d91999f176f2bd83f6fa163b31
HaiMInhLe <haiminh005@gmail.com>
2025-10-14 09:25:57 +0700

    Beginning of the day commit

--- loggrep/cli.py
+++ loggrep/cli.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3 
+
 import argparse
 from .core import read_and_find_normal
 
--- loggrep/core.py
+++ loggrep/core.py
@@ -6,14 +6,13 @@ def read_and_find_normal(document, pattern):
                                                     #the "with" statement is called a context manager,  just a shorter to say open and close the file
 
         for num, line in enumerate(file, start=1):  #Read line by line, and use enumerate to get the index of the line
-            if re.search(pattern, line, re.I):     #Check if the pattern is in the line, and make it case insensitivity
+            if re.search(pattern, line, re.I):      #Check if the pattern is in the line, and make it case insensitivity
                 print(f"{num}: {line.strip()}")                 #Print out the line contains the pattern
 
 #def read_and_find_pepperoni(document, pattern):
 #    with open(document, "r") as file:               #Creat a file object that connect to the actual file, whose path is stored inside "document"
 #        for num, line in enumerate(file, start=1):
 #            if re.
-
 
 
 
